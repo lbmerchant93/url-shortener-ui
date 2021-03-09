60ecea9e39c41c5e73a40a7b43f6ade23c847633
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-09 10:16:12 -0700

    Test that the inputs hold the value typed into them

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -12,4 +12,12 @@ describe('URL Shortener', () => {
     it('Should display all of the existing urls on page load', () => {
         cy.get('.url').should('have.length', '4')
     })
+
+    it('Should store the values in the inputs until they are submitted', () => {
+        cy.get('input').eq(0).type('Peach')
+        cy.get('input').eq(0).should('have.attr', 'value', 'Peach')
+        cy.get('input').eq(1).type('https://www.youtube.com/watch?v=NmT1l5CUfgs&list=RDEMqribv0Mn5Pp2PPKhIenVuQ&index=28')
+        cy.get('input').eq(1).should('have.attr', 'value', 'https://www.youtube.com/watch?v=NmT1l5CUfgs&list=RDEMqribv0Mn5Pp2PPKhIenVuQ&index=28')
+    })
+
 })
