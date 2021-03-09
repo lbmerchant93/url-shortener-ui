e9fd6332a10f4694c69215df41e023a07a98da73
Lucas Merchant <lbmerchant93@gmail.com>
2021-03-09 10:26:51 -0700

    Stub and test for when inputs are submitted the new information is displayed

--- cypress/integration/dashboard-spec.js
+++ cypress/integration/dashboard-spec.js
@@ -6,6 +6,14 @@ describe('URL Shortener', () => {
             body: urls
             })
         })
+        cy.fixture('testUrls.json')
+        .then(data => {
+            cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+            body: data.postUrl,
+            headers: {'Content-Type': 'application/json'},
+            })
+        })
+
         cy.visit('http://localhost:3000/')
     })
     
@@ -18,6 +26,13 @@ describe('URL Shortener', () => {
         cy.get('input').eq(0).should('have.attr', 'value', 'Peach')
         cy.get('input').eq(1).type('https://www.youtube.com/watch?v=NmT1l5CUfgs&list=RDEMqribv0Mn5Pp2PPKhIenVuQ&index=28')
         cy.get('input').eq(1).should('have.attr', 'value', 'https://www.youtube.com/watch?v=NmT1l5CUfgs&list=RDEMqribv0Mn5Pp2PPKhIenVuQ&index=28')
+    })
+
+    it('Should display the new shortened URL when the form is filled out and submitted', () => {
+        cy.get('input').eq(0).type('Between Days')
+        cy.get('input').eq(1).type('https://www.youtube.com/watch?v=qm0ru2iBuB0&list=RDEMqribv0Mn5Pp2PPKhIenVuQ&index=28')
+        cy.get('button').click()
+        cy.get('.url').should('have.length', '5')
     })
 
 })
